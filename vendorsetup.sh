9a69e1faca5f8dad979059cb2ca42d694e1defc9
julival25 <julival.bitencourt@gmail.com>
2021-11-20 21:04:11 -0300

    Tulip: Added vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+#clone Display Hals
+rm -rf hardware/qcom-caf/msm8998/dysplay
+git clone https://github.com/SDM660-Project/android_hardware_qcom_display -b msm8998-4.4 hardware/qcom-caf/msm8998/display
